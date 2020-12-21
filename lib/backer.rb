014b61675b75e8fc2b95270485978cb366cef2f9
Romano Livi <romano_livi@outlook.com>
2020-12-21 17:46:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,9 @@
 class Backer 
-  attr_accessor :name 
+  attr_accessor :name, :backed_projects
   
-  
-  def new 
-    backer = Backer.new
+  def initialize(name)
+    @name = name
+    @backed_projects = []
   end
-  
-  def backed_projects
-    @backed_projects
-  end
-  
   
 end 
